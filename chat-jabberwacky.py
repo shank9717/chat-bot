57c9e2cdf621423ea76d024afb4cd86c8c4321a0
shank9717 <shank9717@users.noreply.github.com>
2016-10-18 18:59:56 +0530

    chat-jabberwacky

---
+++ chat-jabberwacky.py
@@ -0,0 +1,135 @@
+from selenium import webdriver
+import os , time
+import re
+from selenium.webdriver.common.keys import Keys
+
+
+def chat():
+    search_key = input("Who do you want me to chat with? : ")
+    your_name = input("Enter you name (as provided on whatsapp): ")
+    your_number = input("Enter you number on whatsapp (Format- : +xx xxxxx xxxxx")
+    try:
+        url = "https://web.whatsapp.com/"
+        url2 = "https://www.jabberwacky.com/j2reply/"
+
+        options = webdriver.ChromeOptions()
+        chromedriver = "D://chromedriver.exe"
+        os.environ["webdriver.chrome.driver"] = chromedriver
+        options.add_argument(r'--user-data-dir=C:\Users\SHASHANK\AppData\Local\Google\Chrome\User Data')
+        browser = webdriver.Chrome(chromedriver,chrome_options=options)
+        type(browser)
+
+        browser.get(url)
+        time.sleep(3)
+        while True:
+            try:
+                logged_in = browser.find_element_by_css_selector('#side')
+                if logged_in:
+                    break
+                else:
+                    pass
+            except:
+                pass
+            time.sleep(0.5)
+        search_bar = browser.find_element_by_css_selector('#side > div.search-container > div > label > input')
+        search_bar.click()
+        search_bar.send_keys(search_key)
+        if (browser.current_url == url):
+            pass
+        else:
+            browser.close()
+            chat()
+        time.sleep(3)
+        found = browser.find_element_by_xpath('//*[@title="'+ search_key +'"]')
+        found.click()
+        time.sleep(1)
+        browser.find_element_by_css_selector('body').send_keys(Keys.CONTROL + "t")
+        browser.switch_to.window(browser.window_handles[-1])
+        browser.get(url2)
+        if "jabber" in browser.current_url:
+            pass
+        else:
+            browser.close()
+            chat()
+        browser.find_element_by_css_selector('body').send_keys(Keys.CONTROL + "\t")
+        browser.switch_to.window(browser.window_handles[0])
+        if (browser.current_url == url):
+            pass
+        else:
+            browser.close()
+            chat()
+        while True:
+            if (browser.current_url == url):
+                pass
+            else:
+                browser.close()
+                chat()
+            messages = browser.find_elements_by_class_name('bubble-text')
+            for message in messages:
+                id = ""
+                code = message.get_attribute('innerHTML')
+                try:
+                    emoji = re.findall(r'<span class="emojitext selectable-text"><img alt="(.*?)".*?</span>',code)[0]
+                except:
+                    pass
+                my_data_list = re.findall(r'<span class="message-pre-text">(.*?)</span>', code)
+                my_data = my_data_list[0]
+                elements = re.findall(r'<!-- react-text:.*?-->(.*?)<!-- /react-text -->', my_data)
+                try_id = re.findall(
+                    r'<span class="screen-name-text" dir="auto"><!-- react-text:.*?-->(.*?)<!-- /react-text -->', code)
+                if len(try_id) < 1:
+                    id = elements[3]
+                else:
+                    id = try_id[0]
+                his_reply = message.find_element_by_class_name('selectable-text')
+                his_reply = his_reply.text
+            if his_reply=="":
+                his_reply == emoji
+            if (browser.current_url == url):
+                pass
+            else:
+                browser.close()
+                chat()
+            if id == "+91 89716 78401" or id == "Shashank Ullas":
+                continue
+            else:
+                pass
+            browser.find_element_by_css_selector('body').send_keys(Keys.CONTROL + "\t")
+            browser.switch_to.window(browser.window_handles[-1])
+            if "jabber" in browser.current_url :
+                pass
+            else:
+                browser.close()
+                chat()
+            bot_chat = browser.find_element_by_xpath(
+                '/html/body/form/table/tbody/tr/td[1]/table[1]/tbody/tr[10]/td/table/tbody/tr/td[1]/input[1]')
+            bot_chat.send_keys(his_reply)
+            bot_chat.send_keys(Keys.ENTER)
+            while True:
+                bot_reply = browser.find_element_by_xpath(
+                    r'/html/body/form/table/tbody/tr/td[1]/table[1]/tbody/tr[9]/td/table/tbody/tr/td[1]/span').text
+                if bot_reply == his_reply:
+                    pass
+                elif bot_reply == prev_reply:
+                    pass
+                else:
+                    break
+            print(bot_reply)
+            browser.find_element_by_css_selector('body').send_keys(Keys.CONTROL + "\t")
+            browser.switch_to.window(browser.window_handles[0])
+            if (browser.current_url == url):
+                pass
+            else:
+                browser.close()
+                chat()
+            his_chat = browser.find_element_by_css_selector('#main > footer > div.block-compose > div.input-container > div > div.input')
+            his_chat.send_keys(bot_reply)
+            his_chat.send_keys(Keys.ENTER)
+            if his_reply.lower() == "bye":
+                break
+            prev_reply = bot_reply
+        browser.close()
+    except:
+        browser.close()
+        chat()
+chat()
